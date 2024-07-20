e6a9ade0bf804cf14649d3742a299ca7385e38ed
Readeem <Readeem@users.noreply.github.com>
2024-07-20 15:58:19 +0300

    Removed color support check

--- source/output.py
+++ source/output.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-import os
-import sys
 from typing import Any
 
 __all__ = (
@@ -61,48 +59,7 @@ class Output:
             self, *,
             use_color: bool = True
     ) -> None:
-        self.use_color: bool = use_color and self.is_color_supported()
-
-    @staticmethod
-    def is_color_supported() -> bool:
-        """Return True if the terminal device supports color output.
-
-        Returns
-        -------
-        bool
-            True if color is allowed on the output terminal, False otherwise.
-        """
-        # isatty is not always implemented, https://code.djangoproject.com/ticket/6223
-        is_a_tty = hasattr(sys.stdout, 'isatty') and sys.stdout.isatty()
-
-        clicolor = os.environ.get('CLICOLOR')
-        if os.environ.get('CLICOLOR_FORCE', '0') != '0':
-            # https://bixense.com/clicolors/
-            return True
-        if clicolor is not None and clicolor != '0' and is_a_tty:
-            # https://bixense.com/clicolors/
-            return True
-        if clicolor == '0':
-            # https://bixense.com/clicolors/
-            return False
-        if os.environ.get('COLORTERM', '').casefold() in ('truecolor', '24bit'):
-            # Seems to be used by Gnome Terminal.
-            # https://gist.github.com/XVilka/8346728#true-color-detection
-            return True
-
-        plat = sys.platform
-        if plat == 'Pocket PC':
-            return False
-
-        if plat == 'win32':
-            color_support = os.environ.get('TERM_PROGRAM') in ('mintty', 'Terminus')
-            color_support |= 'ANSICON' in os.environ
-            color_support &= is_a_tty
-        else:
-            color_support = is_a_tty
-
-        color_support |= bool(os.environ.keys() & {'PYCHARM_HOSTED', 'WT_SESSION'})
-        return color_support
+        self.use_color: bool = use_color
 
     def info(self, text: str) -> None:
         if self.use_color:
